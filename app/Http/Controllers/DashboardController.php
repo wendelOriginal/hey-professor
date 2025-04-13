ccdf79f804b400aecd6c30a1de578ef973830bd6
wendelOriginal <wendel.original@gmail.com>
2025-04-12 23:44:16 -0300

    HEY-10: alter phpstan.neon type return view dashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): View
     {
         return view('dashboard', [
             'questions' => Question::all(),
