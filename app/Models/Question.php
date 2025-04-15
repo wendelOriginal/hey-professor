7379bab6a6ebf3ee59dba881f59f03ca75a532f4
wendelOriginal <wendel.original@gmail.com>
2025-04-15 18:50:30 -0300

    HEY-10: fix bug

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -19,24 +18,4 @@ class Question extends Model
     {
         return $this->hasMany(LikeQuestion::class);
     }
-
-    // /**
-    //  * @return Attribute<int, $this>
-    //  */
-    // public function likes(): Attribute
-    // {
-    //     return new Attribute(
-    //         get: fn() => $this->likeQuestion()->sum('like')
-    //     );
-    // }
-    // /**
-    //  * @return Attribute<int, $this>
-    //  */
-
-    // public function unlikes(): Attribute
-    // {
-    //     return new Attribute(
-    //         get: fn() => $this->likeQuestion()->sum('unlike')
-    //     );
-    // }
 }
